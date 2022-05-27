1eb4e50e847572270654852a1646f888a24fc654
sang2xx2 <s191102g@gmail.com>
2022-05-27 10:32:31 +0700

    fix db

--- models/db.js
+++ models/db.js
@@ -1,23 +1,23 @@
 var mysql = require('mysql');
-// var db = mysql.createConnection({
-//     host: 'us-cdbr-east-05.cleardb.net',
-//     user: 'b4974887c1c471',
-//     password: '6b304aef',
-//     database: 'heroku_db08387a40c3c0f'
-// } );
-
 var db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'project'
+    host: 'us-cdbr-east-05.cleardb.net',
+    user: 'b5fb007648129a',
+    password: 'feff71a5',
+    database: 'heroku_be5f40939384bb1'
 } );
+
+// var db = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'project'
+// } );
 db.connect(() => console.log('Da ket noi database !'));
 
 db.on('error', function(err) {
     console.log(err.code);
   });
-// setInterval(function () {
-//     db.query('SELECT 1');
-// }, 5000);
+setInterval(function () {
+    db.query('SELECT 1');
+}, 5000);
 module.exports = db; 
